5dabde2a9dea86d396183555b8ceeb4474cc20a9
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-06-07 19:42:43 -0300

    Inclusão da página Join

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -7,48 +7,48 @@ const Projetos = () => {
   return (
     <section className="Projeto-container">
          <h1 className="Projeto-title">Projetos</h1>
-         <p className="Projeto-paragrafo">Descubra como nosso projetos digitais podem ajudá-lo a alcançar seus objetivo de negócios online.</p>
+         <p className="Projeto-paragraph">Descubra como nosso projetos digitais podem ajudá-lo a alcançar seus objetivo de negócios online.</p>
          <div className="Projeto-row">
            <div className="Projeto-col">
             <div className="Projeto-card">
               <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-              <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-              <p className="Projeto-card-paragrafo">Visite a página</p>
+              <h2 className="Projeto-card_title">Nome do Site / Projeto</h2>
+              <p className="Projeto-card_paragraph">Visite a página</p>
             </div>
            </div>
            <div className="Projeto-col">
             <div className="Projeto-card">
               <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-              <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-              <p className="Projeto-card-paragrafo">Visite a página</p>
+              <h2 className="Projeto-card_title">Nome do Site / Projeto</h2>
+              <p className="Projeto-card_paragraph">Visite a página</p>
             </div>
            </div>
             <div className="Projeto-col">
              <div className="Projeto-card">
                <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-              <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-              <p className="Projeto-card-paragrafo">Visite a página</p>
+              <h2 className="Projeto-card_title">Nome do Site / Projeto</h2>
+              <p className="Projeto-card_paragraph">Visite a página</p>
             </div>
            </div>
           <div className="Projeto-col">
            <div className="Projeto-card">
              <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-            <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-            <p className="Projeto-card-paragrafo">Visite a página</p>
+            <h2 className="Projeto-card_title">Nome do Site / Projeto</h2>
+            <p className="Projeto-card_paragraph">Visite a página</p>
            </div>
           </div>
         <div className="Projeto-col">
           <div className="Projeto-card">
             <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-            <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-            <p className="Projeto-card-paragrafo">Visite a página</p>
+            <h2 className="Projeto-card_itle">Nome do Site / Projeto</h2>
+            <p className="Projeto-card_paragraph">Visite a página</p>
           </div>
         </div>
         <div className="Projeto-col">
           <div className="Projeto-card">
             <img className="Projeto-card_imagem" src={card} alt="Imagem do computador" />
-            <h2 className="Projeto-card-title">Nome do Site / Projeto</h2>
-            <p className="Projeto-card-paragrafo">Visite a página</p>
+            <h2 className="Projeto-card_title">Nome do Site / Projeto</h2>
+            <p className="Projeto-card_paragraph">Visite a página</p>
            </div>
         </div>
        </div>
